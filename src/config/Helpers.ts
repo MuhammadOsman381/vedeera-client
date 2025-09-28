64f41dfb17921ad6a17d736364de0de35bac49f0
MuhammadOsman381 <mosman257@gmail.com>
2025-09-28 15:05:46 +0500

    ----

--- src/config/Helpers.ts
+++ src/config/Helpers.ts
@@ -1,7 +1,7 @@
 class Helpers {
     static localhost: string = 'localhost:3000';
     static server: string = '46.202.140.156:3000';
-    static basePath: string = `http://${this.server}`;
+    static basePath: string = `https://${this.server}`;
     static apiUrl: string = `${this.basePath}/api/`;
     static secretKey: string = '3434774438';
 }
